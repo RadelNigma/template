4a267b0c4283d6c504f43c9da40ef42d85b1be1d
RadelNigma <radelnigma@yandex.ru>
2023-05-20 02:50:39 +0300

    creat config_server

---
+++ config-server/src/main/java/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
