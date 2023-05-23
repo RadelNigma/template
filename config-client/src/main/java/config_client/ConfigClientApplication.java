90de559a687912b2ffa08fd7b804df33313a0b38
RR.Nigmatullin <radelnigma@yandex.ru>
2023-05-23 18:10:40 +0300

    23.05.23

--- config-client/src/main/java/config_client/ConfigClientApplication.java
+++ config-client/src/main/java/config_client/ConfigClientApplication.java
@@ -1,15 +1,13 @@
-package config_server;
+package config_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class ConfigClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(ConfigClientApplication.class,args);
     }
 }
--- eureka-server/src/main/java/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/eureka_server/EurekaServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ *  Содежрит реестр служб и REST API, которые можно использовать для регистрации службы,
+ *  отмены регистрации службы и определения местоположения других служб.
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
