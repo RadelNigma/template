1786fef05f7d3d6d251a47ff7094ca6d6e14983e
RR.Nigmatullin <radelnigma@yandex.ru>
2023-05-24 17:58:40 +0300

    Add ServiceA and ServiceB

---
+++ ServiceB/src/main/java/org/example/restcontroller/BookController.java
@@ -0,0 +1,15 @@
+package org.example.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @GetMapping
+    public String getString() {
+        return "My String from feign client";
+    }
+}
